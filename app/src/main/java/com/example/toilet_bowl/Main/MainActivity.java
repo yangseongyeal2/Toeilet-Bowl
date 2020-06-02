e2addbaee5c8e3a8e65596412593eadc884bfbbf
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2020-06-02 22:04:51 +0900

    주석 완료

--- app/src/main/java/com/example/toilet_bowl/Main/MainActivity.java
+++ app/src/main/java/com/example/toilet_bowl/Main/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private mypage mypage;
     private FirebaseFirestore mStore=FirebaseFirestore.getInstance();
 
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
