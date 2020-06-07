e4f8880d304d16aab0b1666f0f269106ec4daa9b
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2020-06-08 01:46:01 +0900

    마이페이지 완료

--- app/src/main/java/com/example/toilet_bowl/Main/Fragment/mypage.java
+++ app/src/main/java/com/example/toilet_bowl/Main/Fragment/mypage.java
@@ -1,16 +1,21 @@
 package com.example.toilet_bowl.Main.Fragment;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.example.toilet_bowl.Main.MainActivity;
 import com.example.toilet_bowl.R;
 import com.example.toilet_bowl.model.FirebaseUserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,9 +30,11 @@ public class mypage extends Fragment {
     private TextView  mEmail;
     private TextView  mNickname;
     private TextView  mUserNickname;
-    private TextView  mLikecount;
-    private final FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+    private TextView  mLikecount,mCreatedAt;
     private FirebaseFirestore mStore=FirebaseFirestore.getInstance();
+    private FirebaseUser firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+
+
 
     @Nullable
     @Override
@@ -37,7 +44,48 @@ public class mypage extends Fragment {
         mNickname=view.findViewById(R.id.mypage_Nickname);
         mUserNickname=view.findViewById(R.id.mypage_UserNickname);
         mLikecount=view.findViewById(R.id.mypage_likecount);
+        mCreatedAt=view.findViewById(R.id.mypage_created_at);
         RetrieveALL();
+        view.findViewById(R.id.mypage_changeNick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad=new AlertDialog.Builder(getContext());
+                ad.setMessage("바꾸고 싶은 닉네임 입력");
+                final EditText et=new EditText(getContext());
+                et.setHint("13글자 이내로 작성하시오");
+                ad.setView(et);
+                ad.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, int which) {
+
+                        String result=et.getText().toString();
+                        if(result.length()>13){
+                            Toast.makeText(getContext(),"13글자이하로 작성하시오",Toast.LENGTH_LONG).show();
+                        }else{
+                            mStore.collection("users").document(firebaseUser.getUid())
+                                    .update("userNickName",result)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            RetrieveALL();
+                                            dialog.dismiss();
+                                        }
+                                    });
+                        }
+
+                    }
+                });
+
+                ad.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {//
+                        dialog.dismiss();
+                    }
+                });
+                ad.show();
+
+            }
+        });
         return view;
     }
 
@@ -54,6 +102,8 @@ public class mypage extends Fragment {
                     mNickname.setText("현재 레벨"+fm.getNickname());
                     mUserNickname.setText("닉네임:"+fm.getUserNickName());
                     mLikecount.setText("좋아요 받은 갯수:"+String.valueOf(fm.getLikecount()));
+                    String date="생성된 날짜: "+fm.getDate().toString().substring(0,13);
+                    mCreatedAt.setText(date);
                 }
 
 
